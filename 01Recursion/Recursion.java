2e9b3d3245694e78e4b484ffbaf317623867cd89
jsun5 <jsun5@stuy.edu>
2018-02-03 22:44:40 -0500

    wrote fib and sqrt, doesnt work w small numbers

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -26,6 +26,24 @@ public class Recursion{
         }
         return fibber (n - 1, sum + sumBefore, sum);
     }
+    
+    public double sqrt(double n){
+        if (n < 0) {
+            throw new IllegalArgumentException("please choose a number larger than 0");
+        }
+        if (n == 0){
+            return 0;
+        }
+        return sqrter(n, n / 2);
+    }
+    
+    public double sqrter(double n, double guess){
+        if ( ((guess * guess - n) / n) < .000000001){
+            return guess;
+        }
+        //System.out.println(guess);
+        return sqrter(n, ( n / guess + guess) / 2);
+    }
         
     public static void main(String[]args){
         Recursion test = new Recursion();
@@ -35,6 +53,8 @@ public class Recursion{
         System.out.println(test.fact(1));
         System.out.println(test.fact(5));
         System.out.println(test.fact(10));
+        
+//        System.out.println(test.fib(-1));
         System.out.println(test.fib(0));
         System.out.println(test.fib(1));
         System.out.println(test.fib(2));
@@ -45,5 +65,15 @@ public class Recursion{
         System.out.println(test.fib(7));
         System.out.println(test.fib(8));
         System.out.println(test.fib(9));
+        
+//        System.out.println(test.sqrt(-1));
+        System.out.println(test.sqrt(0));
+        System.out.println(test.sqrt(1));
+        System.out.println(test.sqrt(2));
+        System.out.println(test.sqrt(3));
+        System.out.println(test.sqrt(4));
+        System.out.println(test.sqrt(256));
+        System.out.println(test.sqrt(260));
+        
     }
 }
