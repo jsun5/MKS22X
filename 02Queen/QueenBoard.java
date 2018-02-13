19a5d997be9d92623036a03dbe726bcf80ca8551
jsun5 <jsun5@stuy.edu>
2018-02-12 21:05:37 -0500

    code works! commented out main testing

--- 02Queen/QueenBoard.java
+++ 02Queen/QueenBoard.java
@@ -12,6 +12,12 @@ public class QueenBoard{
         addDirection(1,r,c,1,0);
         addDirection(1,r,c,1,1);
         addDirection(1,r,c,1,-1);
+//        addDirection(1,r,c,-1,0);
+  //      addDirection(1,r,c,-1,1);
+    //    addDirection(1,r,c,-1,-1);
+        addDirection(1,r,c,0,1);
+       // addDirection(1,r,c,0,-1);
+        
         board[r][c] = -1;
         return true;
     }
@@ -23,30 +29,31 @@ public class QueenBoard{
         addDirection(-1,r,c,1,0);
         addDirection(-1,r,c,1,1);
         addDirection(-1,r,c,1,-1);
+        addDirection(-1,r,c,0,1);
         board[r][c] = 0;
         return true;
     }
 
     private void addDirection(int toBeAdded,int r, int c, int xdir, int ydir){
-        while (r > 0 && r < board.length && c < board[0].length){
+        while (r >= 0 && c >= 0 && r < board.length && c < board[0].length){
             board[r][c] += toBeAdded;
             r += ydir;
             c += xdir;
         }
-    }
-
+    } 
+    
     public String toString(){
         String ans = "";
         for (int r = 0; r < board.length; r ++){
             for (int c = 0; c <board[0].length; c++){
-                /*if (board[r][c] < 0){
+                if (board[r][c] < 0){
                     ans += "Q ";
                 }
                 else{
                     ans += "_ ";
-                }*/
-             ans += board[r][c] + " ";}
-            ans += "\n";
+                }
+            }
+            ans += "\n"; 
         }
         return ans;
     }
@@ -56,6 +63,7 @@ public class QueenBoard{
     //    if (board.length == 2 || board.length == 3 ){
   //          return false;
 //        }
+
         for (int r = 0; r < board.length; r++){
             for (int c = 0; c < board.length; c++){
                 if (board[r][c] != 0){
@@ -63,10 +71,14 @@ public class QueenBoard{
                 }
             }
         }
+        if (board.length == 0){
+            return true;
+        }
         return solver(0);
     }
     
     public boolean solver(int c){
+
         if (c == board[0].length){
             return true;
         }
@@ -91,42 +103,44 @@ public class QueenBoard{
                 }
             }
        }
+        if (board.length == 0){
+            return 1;
+        }
        return counter(0);
     }
    
     public int counter(int c){
-	int total = 0;
+        int total = 0;
         if (c == board[0].length){
                 return 1;
-	}
-
+        }
         for (int r = 0; r < board.length; r++){
             if (addQueen(r,c)){
                 total += counter(c + 1);
                 removeQueen(r,c);
             }
-	}
+        }
         return total;
 }
 
 
-    public static void main(String[]args){
+/*    public static void main(String[]args){
         QueenBoard test = new QueenBoard(8);
         System.out.println(test);
-        test.addQueen(4,0);
+        test.addQueen(4,4);
 	   System.out.println(test);
 	   test.addQueen(7,5);
 	   System.out.println(test);
 	   test.addQueen(6,1);
 	   System.out.println(test);
-	// test.removeQueen(4,4);
-	/*       System.out.println(test);
-	System.out.println(test.solve());
-        System.out.println(test);
+	// test.removeQueen(4,4);  
+	       System.out.println(test);
+	//System.out.println(test.solve());
+      //  System.out.println(test);
         
         QueenBoard test2 = new QueenBoard(2);
         System.out.println(test2.solve());
-        System.out.println(test2);
+        System.out.println(test2); 
         
 	    QueenBoard b = new QueenBoard(4);
 
@@ -145,10 +159,10 @@ public class QueenBoard{
       System.out.println("Error: The board contains non-zero values");
     } //prints "Error: The board contains non-zero values"
 
-    for (int i = 1; i < 12; i++){
+    for (int i = 0; i < 12; i++){
       QueenBoard a = new QueenBoard(i);
       System.out.println("# of Solutions for " + i + ": " + a.countSolutions());
-      /*         Expected Values
+               Expected Values
        i --> # of Solutions   i --> # of Solutions
       0 --> 1                      6 --> 4
       1 --> 1                      7 --> 40
@@ -156,8 +170,8 @@ public class QueenBoard{
       3 --> 0                      9 --> 352
       4 --> 2                    10 --> 724
       5 --> 10                  11 --> 2680
-      */
-      // System.out.println(a); //prints out an empty i by i grid of underscores 
-     
+      
+       System.out.println(a); //prints out an empty i by i grid of underscores       
     }
+    }*/
 }
