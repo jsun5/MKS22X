239d7c1c7f7d9bc91449f1939c4fc4299837babc
jsun5 <jsun5@stuy.edu>
2018-02-15 14:46:03 -0500

    wrote more

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -1,30 +1,64 @@
 public class KnightBoard{
     private int[][] board;
-    private int[][] moveSet = {{-2,1}
-			       {-2,-1}
-			       {-1,2}
-			       {-1,-2}
-	                       {1,2}
-			       {1,-2}
-			       {2,1}
+    private int[][] moveSet = {{-2,1},
+			       {-2,-1},
+			       {-1,2},
+			       {-1,-2},
+	                       {1,2},
+			       {1,-2},
+			       {2,1},
 			       {2,-1}};
 
     public KnightBoard(int startingRows, int startingCols){
+	if (startingRows < 0 || startingCols < 0){
+	    throw new IllegalArgumentException("both args > 0");
+	}
 	board = new int[startingRows][startingCols];
     }
 
-    public String toString(){}
+    public String toString(){
+	String ans = "";
+	for (int r = 0; r < board.length; r ++){
+	    for (int c = 0; c < board[0].length; c ++){
+		ans += board[r][c] + " ";
+	    }
+	    ans += "\n";
+	}
+	return ans;
+    }
 
     public boolean solve(int startR, int startC){
-	solver(startR, startC, 1);
+	return solver(startR, startC, 1);
     }
 
     private boolean solver(int row, int col, int level){
 	board[row][col]= level;
-	int[] validMoves = new int[16];
+		if (level == board.length * board[0].length){
+	  return true;
+	}
+	for (int i = 0; i < moveSet.length; i++){
+	    try{
+		if (board[row + moveSet[i][0]] [col + moveSet[i][1]] == 0){
+		    if( solver(row + moveSet[i][0], col + moveSet[i][1], level + 1)){
+			return true;
+		    }
+		       else{
+			   board[row][ col] = 0;
+		     }
+		}
+			
+	    }
+	    catch(ArrayIndexOutOfBoundsException e){}
+	}	
+	return false;
     }
 
-    private boolean  validate(int row, int col){
-	for (int i = 0; i < moveSet.length; i++){
-	    if (board[row + moveSet[i][0]] [col + moveSet[i][1]] == 0){
+    public static void main( String[]args){
+	KnightBoard test = new KnightBoard(3,5);
+      	System.out.println(test);
+	System.out.println(test.solve(0,0));
+	System.out.println(test);
+    }
+}
+
 		
