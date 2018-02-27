58f5361c24d3e95bd6064c709c037f49a75ec500
jsun5 <jsun5@stuy.edu>
2018-02-27 14:46:27 -0500

    writing constructor

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -1,9 +1,14 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Maze {
-    public Maze(){}
+
+    private char[][] maze;
+    private boolean animate;
+    
+    public Maze(String filename){
+	animate = false;
+    }
     
     public void ReadFile (String fileName){
         try{
@@ -14,20 +19,104 @@ public class Maze {
         //inf stands for the input file
         Scanner inf = new Scanner(text);
 
-        String ans;
+        String ans = "";
         int cols;
-        int rows;
+        int rows = 0;
         while(inf.hasNextLine()){
             String line = inf.nextLine();
             cols = line.length();
+	    rows++;
+	    ans += line + "/";
             System.out.println(line);//hopefully you can do other things with the line
-        }       
         }
+	maze = new char[rows][cols];
+	int i = 0;
+	for (int r = 0; r < rows; r ++){
+	    for (int c = 0; c < cols; c++){
+		if (ans.charAt(i) != '/'){
+		    maze[r][c] = ans.charAt(i);
+		}
+		i++;
+	
+	    }
+	}
+	}
         catch (FileNotFoundException e){}
-    }   
-    
-    public static void main(String[]args){
-        Maze test = new Maze();
-        test.ReadFile("Maze1.txt");
     }
+       
+    public String toString(){
+	String ans = "";
+	for (int r = 0; r < maze.length; r ++){
+	    for (int c = 0; c < maze[0].length; c++){
+		ans += maze[r][c];
+	    }
+	    ans += "\n";
+	}
+	return ans;
+    }
+     private void wait(int millis){
+         try {
+             Thread.sleep(millis);
+         }
+         catch (InterruptedException e) {
+         }
+     }
+
+    public void setAnimate(boolean b){
+        animate = b;
+    }
+
+    public void clearTerminal(){
+        //erase terminal, go to top left of screen.
+        System.out.println("\033[2J\033[1;1H");
+    }
+
+
+    /*        Wrapper Solve Function returns the helper function
+      Note the helper function has the same name, but different parameters.
+      Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
+    
+      public int solve(){
+            //find the location of the S. 
+
+            //erase the S
+
+            //and start solving at the location of the s.
+            //return solve(???,???);
+    }
+
+    
+      Recursive Solve function:
+
+      A solved maze has a path marked with '@' from S to E.
+
+      Returns the number of @ symbols from S to E when the maze is solved,
+      Returns -1 when the maze has no solution.
+
+      Postcondition:
+        The S is replaced with '@' but the 'E' is not.
+        All visited spots that were not part of the solution are changed to '.'
+            Note: This is not required based on the algorithm, it is just nice visually to see.
+        All visited spots that are part of the solution are changed to '@'
+    
+    private int solve(int row, int col){ //you can add more parameters since this is private
+
+        //automatic animation! You are welcome.
+        if(animate){
+            clearTerminal();
+            System.out.println(this);
+            wait(20);
+        }
+
+        //COMPLETE SOLVE
+        return -1; //so it compiles
+    }
+
+}*/
 }
+    
+  public static void main(String[]args){
+	     Maze test = new Maze("data1.dat");
+	     maze.ReadFile("data1.dat");
+    }
+
