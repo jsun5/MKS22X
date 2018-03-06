c900164f87912728fd598989fa5be431bc052420
jsun5 <jsun5@stuy.edu>
2018-03-06 14:45:37 -0500

    skeleton

---
+++ 05USACO/USACO.java
@@ -0,0 +1,17 @@
+public class USACO{
+
+    private int[][] elevations =
+	    {{28 25 20 32 34 36},
+	     {27 25 20 20 30 34},
+	     {24 20 20 20 20 30},
+	     {20 20 14 14 20 20}};
+    
+
+
+	
+    public static int bronze(){
+    }
+
+    private void stomp(int R_s, int C_s, int D_s){
+	
+    
