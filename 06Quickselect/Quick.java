617801197309fee0d8a342cb87e485861a117a9a
jsun5 <jsun5@stuy.edu>
2018-03-15 14:45:30 -0400

    skeleton

--- 06Quickselect/Quick.java
+++ 06Quickselect/Quick.java
@@ -1,6 +1,23 @@
 public class Quick{
 
-    public int partition(int[] data, int start, int end){
-	int pivot = start;
+    public int partition(int[] data, int low, int hi){
+	int pivot = low;
+	int i = low;
+	int j = hi;
+	swap (data,i,pivot);
+	i++;
+	while(i <= j){
+	    if (data[i] < data[low]){
+		i++;
+	    }
+	    if (data[i] > data[low]){
+		swap(data, i, j);
+		j--;
+	    }
+	}
+        swap (data,j,low);
+        return j;	
+    }
+}
 	
 	
