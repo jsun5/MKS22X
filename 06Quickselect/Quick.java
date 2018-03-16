6ee0d65a7d598a82a23052cc908647655cce9eef
jsun5 <jsun5@stuy.edu>
2018-03-16 11:42:38 -0400

    almost done w partition

--- 06Quickselect/Quick.java
+++ 06Quickselect/Quick.java
@@ -1,7 +1,8 @@
 public class Quick{
 
-    public int partition(int[] data, int low, int hi){
-	int pivot = low;
+    public static int partition(int[] data, int low, int hi){
+	int pivot = (int) (Math.random() * (hi - low)) + low;
+	System.out.println(pivot);
 	int i = low;
 	int j = hi;
 	swap (data,i,pivot);
@@ -14,9 +15,33 @@ public class Quick{
 		swap(data, i, j);
 		j--;
 	    }
+	    print(data);
+	    System.out.println("i: " + i);
+	    System.out.println("j: " + j);
 	}
         swap (data,j,low);
         return j;	
+    }
+
+    public static void print(int[]data){
+	String ans = "{";
+	for (int i = 0; i < data.length; i++){
+	    ans+= data[i] + ", ";
+	}
+	ans+= "}";
+	System.out.println(ans);
+    }
+
+	
+    public static void swap (int[]data,int a, int b){
+	int x = data[a];
+	data[a] = data[b];
+	data[b] = x;
+    }
+
+    public static void main(String[]args){
+	int[] ex = {0,5,2,3,1};
+	System.out.println(Quick.partition(ex,0,4));
     }
 }
 	
