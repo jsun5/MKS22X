38f80cc395b958c316287f499ed4ee8872a50a48
jsun5 <jsun5@stuy.edu>
2018-03-20 14:45:46 -0400

    changed merge, doesntw work anymore ://

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,11 +1,12 @@
-public class Mergesort{
+public class Merge{
     
-    public static int[] merge(int[]data,int s1, int e1, int s2, int e2){
-        int[] ans = new int[data.length];
-        int i1 = s1;
-        int i2 = s2;
+    public static int[] merge(int[]data, int[]ans, int lo, int mid, int hi){
+	ans = new int[data.length];
+        int i1 = lo;
+	mid = (lo + hi) / 2;
+	int i2 = mid + 1;
         int counter = 0;
-        while (i1 < e1 && i2 < e2){
+        while (i1 < mid && i2 < hi){
             if (data[i1] < data[i2]){
                 ans[counter] = data[i1];
                 i1 ++;
@@ -16,14 +17,14 @@ public class Mergesort{
             }
             counter ++;
         }
-        if (i1 == e1){
-            for(int i = i2; i < e2; i++){
+        if (i1 == mid){
+            for(int i = i2; i < hi; i++){
                 ans[counter] = data[i];
                 counter ++;
             }
         }
         else{
-            for (int i = i1; i < e1; i++){
+            for (int i = i1; i < mid; i++){
                 ans[counter] = data[i];
                 counter++;
             }
@@ -41,6 +42,7 @@ public class Mergesort{
     
     public static void main(String[]args){
         int[] tester = {2,4,7,1,2,2,5,6,8};
-        print(merge(tester,0,3,3,9));
+	int[] temp = {};
+        print(merge(tester,temp,0,4,9));
     }
 }
