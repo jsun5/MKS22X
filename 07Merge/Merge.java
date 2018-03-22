49af2b28b3a2817f09d32b8a159f6e7be90e7a63
jsun5 <jsun5@stuy.edu>
2018-03-21 22:35:26 -0400

    wrote mergesort; doesnt work

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,17 +1,25 @@
 public class Merge{
     
-//    public static void mergesort(int[]data){
-  //      msort(data, new int[data.length), 0, data.length);
- //   }
+    public static void mergesort(int[]data){
+        msort(data, new int[data.length], 0, data.length );
+    }
     
-   // public static void (int[]data,int[]temp,int lo, int hi){
-        
-    //}
+    public static void msort(int[]data,int[]temp,int lo, int hi){
+        if (lo >= hi){
+            return;
+        }
+        for (int i = lo; i < hi; i++){
+            temp[i] = data[i];
+        }
+        int mid = (lo + hi) /2;
+        msort(temp, data, lo, mid );
+        msort(temp, data, mid + 1, hi);
+        merge(data, temp, lo, mid , hi);
+    }
     
-    public static int[] merge(int[]data, int[]ans, int lo, int mid, int hi){
-        ans = new int[data.length];
+    public static void merge(int[]data, int[]ans, int lo, int mid, int hi){
+        mid --;
         int i1 = lo;
-        mid = (lo + hi) / 2 - 1;
         int i2 = mid;
         int counter = 0;
         System.out.println("i1: " + i1 + "mid: " + mid  + "i2: " + i2);
@@ -33,12 +41,11 @@ public class Merge{
             }
         }
         else{
-            for (int i = i1; i < mid; i++){
+            for (int i = i1; i < mid ; i++){
                 ans[counter] = data[i];
                 counter++;
             }
         }
-        return ans;
     }
     
     public static void print(int[]data){
@@ -51,7 +58,11 @@ public class Merge{
     
     public static void main(String[]args){
         int[] tester = {2,4,7,1,2,2,5,6,8};
-        int[] temp = {};
-        print(merge(tester,temp,0,3,9));
+        int[] temp = {0,0,0,0,0,0,0,0,0};
+        //merge(tester,temp,0, 9/2,9);
+        //print(temp);
+        mergesort(tester);
+        print(tester);
+        
     }
 }
