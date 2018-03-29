a16478fd9bdb0fe186f52bf5a14a1e473777e533
jsun5 <jsun5@stuy.edu>
2018-03-28 23:15:25 -0400

    middle of stage 1

--- 08LinkedLists/MyLinkedList.java
+++ 08LinkedLists/MyLinkedList.java
@@ -3,65 +3,122 @@ public class MyLinkedList{
     private int length;
     
     public MyLinkedList(){
-	first = null;
-	last = null;
-	length = 0;
+        first = null;
+        last = null;
+        length = 0;
     }
+    
+    public int getLength(){
+        return length;
+    }
+    
+    public Node getFirst(){
+        return first;
+    }
+    
+    public Node getLast(){
+        return last;
+    }
+    
+    public void Longer(){
+        length++;
+    }
+    
+    public void setFirst(Node n){
+        first = n;
+    }
+    
+    public void setLast(Node n){
+        last = n;
+    }
+    
+    public boolean add(int value){
+        if(getLength() == 0){
+            setFirst(new Node(value));
+            Longer();
+            return true;
+        }
+        if(getLength() == 1){
+            setLast(new Node(value, getFirst, null);
+            first.setNext(last);
+            Longer();
+            return true;
+        }
+        for (int i = 0; i < getLength(); i++){
+            current = current.getNext();
+        }
+        return true;
+    }
+        
+        
 
     public int get(int n){
-	if (n == length-1){
-	    return last;
-	}
-	Node current = first;
-	for (int i = 0; i < n; i++){
-	    current = current.getNext();
-	}
-	return current.getValue();
+        if (n == length-1){
+            return last;
+        }
+        Node current = first;
+        for (int i = 0; i < n; i++){
+            current = current.getNext();
+        }
+        return current.getValue();
     }
 
-    public void set(int index, int value){
-	if (n == length-1){
-	    last = new Node(value);
-	}
-	
+    public int set(int index, int value){
+        if (n == length-1){
+            last = new Node(value);
+        }
+        int prev = 0;
+        Node current = first;
+        for (int i = 0; i < n; i++){
+            current = current.getNext();
+        }
+        prev = current.getValue();
+        
+    }
 
     private class Node{
-	Node next,prev;
-	int data;
+        Node next,prev;
+        int data;
 
-	public Node(int d){
-	    data = d;
-	    next = null;
-	    prev = null;
-	}
+        public Node(int d){
+            data = d;
+            next = null;
+            prev = null;
+        }
+        
+        public Node(int d, Node p, Node n){
+            data = d;
+            next = n;
+            prev = p;
+        }
 	    
-	private Node getNext(){
-	    return next;
-	}
+        private Node getNext(){
+            return next;
+        }
 
-	private Node getPrev(){
-	    return prev;
-	}
+        private Node getPrev(){
+            return prev;
+        }
 
-	private int getValue(){
-	    return data;
-	}
+        private int getValue(){
+            return data;
+        }
 
-	private void setNext(Node n){
-	    next = n;
-	}
+        private void setNext(Node n){
+            next = n;
+        }
 
-	private void setPrev(Node p){
-	    prev = p;
-	}
+        private void setPrev(Node p){
+            prev = p;
+        }
 
-	private void setValue(int d){
-	    data = d;
-	}   
+        private void setValue(int d){
+            data = d;
+        }   
 
-	private String toString(){
-	    return "" + data;
-	}
+        private String toString(){
+            return "" + data;
+        }
     }
 	    
 	
