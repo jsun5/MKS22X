408d80585df61d864bc9185540e8dfed5362d1a6
jsun5 <jsun5@stuy.edu>
2018-03-29 14:43:49 -0400

    continue P1, need getNode done

--- 08LinkedLists/MyLinkedList.java
+++ 08LinkedLists/MyLinkedList.java
@@ -7,7 +7,18 @@ public class MyLinkedList{
         last = null;
         length = 0;
     }
-    
+
+    private Node getNode(int index){
+	Node current = first;
+        for (int i = 0; i < n; i++){
+            current = current.getNext();
+        }
+	return current;
+    }
+
+    public void clear(){
+    }
+	
     public int getLength(){
         return length;
     }
@@ -34,19 +45,19 @@ public class MyLinkedList{
     
     public boolean add(int value){
         if(getLength() == 0){
-            setFirst(new Node(value));
+	    Node only = new Node(value);
+            first = only;
+	    last = only;
             Longer();
             return true;
         }
         if(getLength() == 1){
-            setLast(new Node(value, getFirst, null);
+            last = new Node(value, first, null);
             first.setNext(last);
             Longer();
             return true;
         }
-        for (int i = 0; i < getLength(); i++){
-            current = current.getNext();
-        }
+	getNode(getLength()-1).setNext(new Node(value, getNode(getLength()-2)));
         return true;
     }
         
