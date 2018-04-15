cc2a23f1b6638774f58bb7098a301387fc06d4cf
jsun5 <jsun5@stuy.edu>
2018-04-15 16:37:11 -0400

    can place nums in buckets

--- 08LinkedLists/Sort.java
+++ 08LinkedLists/Sort.java
@@ -1,13 +1,31 @@
 public class Sort{
+    
     public static void radixsort(MyLinkedListImproved<Integer> data){
         MyLinkedListImproved<Integer> list = data;
         @SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] buckets = new MyLinkedListImproved[10];
-        int digit = 0;
-        Node current = list.getNode(0);
-        while(current.getValue() != null){
-            int num = current.getValue();
-            buckets[num % digit / 10].add(num);
-            current = current.getNext();
+        for(int i = 0; i < 10; i++){
+            buckets[i] = new MyLinkedListImproved<>();
         }
+        int digit = 10;
+        for(Integer num : list){
+            buckets[num % digit / (digit / 10)].add(num);
+        }
+        print(buckets);
+    }
+    
+    public static void print(MyLinkedListImproved<Integer>[] data){
+        for (int i = 0; i < 10; i++){
+            System.out.println("" + i + data[i]);
+        }
+    }
+            
+    
+    public static void main(String[]args){
+        MyLinkedListImproved<Integer> test = new MyLinkedListImproved<>();
+        test.add (123);
+        test.add (141);
+        System.out.println(test);
+        radixsort(test);
+        System.out.println(test);
     }
 }
