4f0e6e71d4d260b1bd9d38ce7e811d366fb676d7
jsun5 <jsun5@stuy.edu>
2018-04-18 23:47:11 -0400

    finished calc

--- 09Eval/Calculator.java
+++ 09Eval/Calculator.java
@@ -4,34 +4,40 @@ public class Calculator{
 	
     public static double eval(String s){
 	String[] data = s.split(" ");
+//    System.out.println(Arrays.toString(data));
 	LinkedList<Double> stack = new LinkedList<>();
 	//double sum = 0;
 	for (int i = 0; i< data.length; i++){
 	    if(data[i].equals("+")){
 		stack.push(stack.pop() + stack.pop());
 	    }
-	    if(data[i].equals("-")){
-		stack.push(stack.pop() - stack.pop());
+	    else if(data[i].equals("-")){
+            double sub = stack.pop();
+		stack.push(stack.pop() - sub);
 	    }
-	    if(data[i].equals("*")){
+	    else if(data[i].equals("*")){
 		stack.push(stack.pop() * stack.pop());
 	    }
-	    if(data[i].equals("/")){
+	    else if(data[i].equals("/")){
 		double divisor = stack.pop();
 		stack.push(stack.pop() / i);
 	    }
-	    if(data[i].equals("%")){
+	    else if(data[i].equals("%")){
 		double divisor = stack.pop();
 		stack.push(stack.pop() % i);
 	    }
 	    else{
 		stack.push(Double.parseDouble(data[i]));
 	    }	
+//        System.out.println("i: " + i);
+//        System.out.println("Stack: " + stack);
 	}
 	return stack.pop();
     }
 
     public static void main(String[]args){
-	System.out.println(eval("10 2.0 +"));
+        System.out.println(eval("10 2.0 +"));
+        System.out.println(eval("11 3 - 4 + 2.5 *"));
+        System.out.println(eval("8 2 + 99 9 - * 2 + 9 -"));
     }
 }
