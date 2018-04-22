ffd1a8e5c661a0afc5ae010d3593cefa0245ef04
jsun5 <jsun5@stuy.edu>
2018-04-21 20:54:03 -0400

    all methods, adds need fix

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
 public class MyDeque<T>{
-    int size, front, back;
-    T[] data;
+    private int size, front, back;
+    private T[] data;
     
     @SuppressWarnings("unchecked")
     public MyDeque(){
         front = 1;
         back = 0;
-        size = 10;
+        size = 0;
         data = (T[])new Object[10];
     }
     
@@ -19,7 +19,7 @@ public class MyDeque<T>{
         }
         front = 1;
         back = 0;
-        size = initialCapacity;
+        size = 0;
         data = (T[])new Object[initialCapacity];
     }
     
@@ -31,35 +31,71 @@ public class MyDeque<T>{
         if (elem == null){
             throw new NullPointerException();
         }
-        int index;
-        if (front == 0){
-            index = size() - 1;
+        if(size() == data.length){
+            resize();
         }
-        else{
-            index = front - 1;
-        }
+        int index = (front + data.length - 1) % data.length ;
         if(data[index] == null){
             data[index] = elem;
             front = index;
+            size++;
         }
-        //else{resize();}
     }
     
     public void addLast(T elem){
         if(elem == null){
             throw new NullPointerException();
         }
+        if(size()==data.length) {
+            resize();
+        }
         int index = (back + 1) % size();
         if (data[index] == null){
             data[index] = elem;
             back = index;
         }
-        //else resize
     }
     
-    //public void resize(){
-      //  T[] temp = new T[size() * 2];
-     //   for(int i = head; i < hea
+    @SuppressWarnings("unchecked")
+    public void resize(){
+        T[] temp = (T[])new Object[size() * 2];
+        int counter = 0;
+        int i;
+        for(i = front; i < size() + back; i++){
+            temp[counter] = data[i];
+        }
+        front = 0;
+        back = counter;
+        data = temp;
+    }
+    
+    public T getFirst(){
+        if(size() == 0){
+            throw new NoSuchElementException();
+        }
+        return data[front];
+    }
+    
+    public T getLast(){
+        if(size() == 0){
+            throw new NoSuchElementException();
+        }
+        return data[back];
+    }
+    
+    public T removeFirst(){
+        T value = getFirst();
+        data[front] = null;
+        front++;
+        return value;
+    }
+    
+    public T removeLast(){
+        T value = getLast();
+        data[back] = null;
+        back--;
+        return value;
+    }
     
     public String toString(){
         return Arrays.toString(data);
@@ -68,9 +104,10 @@ public class MyDeque<T>{
     public static void main(String[]args){
         MyDeque<Integer> test= new MyDeque<>();
         System.out.println(test);
-        for(int i = 0; i < 6; i++){
+        for(int i = 0; i < 15; i++){
             test.addFirst(1);
             test.addLast(2);
+            System.out.println(test);
         }
         System.out.println(test);
     }
