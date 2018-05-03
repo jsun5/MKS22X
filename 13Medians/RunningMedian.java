6062fc770d9ba036e4450dd7f9950780fa657113
jsun5 <jsun5@stuy.edu>
2018-05-02 23:30:01 -0400

    medians skeelton

---
+++ 13Medians/RunningMedian.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class RunningMedian{
+    private Double[] data;
+    private int size();
+    
+    public RunningMedian(){
+        data = new Double[10];
+    }
+    
+    public void add(Double){
+    }
+    
+    public Double getMedian(){
+        return 0;
+    }
+    
+    public int size(){
+        return size;
+    }
+    
+    public String toString(){
+        return Arrays.toString(data);
+    }
+    
+    public void resize(){};
+    
+}
