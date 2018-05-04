09aedb3d1957d4d4dbbf29edf09dd841e72331db
jsun5 <jsun5@stuy.edu>
2018-05-03 22:32:21 -0400

    started  medians, removed prints

--- 13Medians/RunningMedian.java
+++ 13Medians/RunningMedian.java
@@ -1,18 +1,38 @@
 import java.util.*;
 
 public class RunningMedian{
-    private Double[] data;
-    private int size();
+    private MyHeap<Double> less, more;
+    private int size;
     
     public RunningMedian(){
-        data = new Double[10];
+        less = new MyHeap<Double>();
+        more = new MyHeap<Double>(false);
+        size = 0;
     }
     
-    public void add(Double){
+    public void add(Double data){
+        if(more.size() > less.size() + 1){
+            less.add(more.remove());
+        }
+        else if(size() == 0 || data < less.peek()){
+            less.add(data);
+        }
+        else if(data > less.peek()){
+            more.add(data);
+        }
+        size++;
     }
     
     public Double getMedian(){
-        return 0;
+        if(less.size() > more.size()){
+            return less.peek();
+        }
+        else if (less.size() < more.size()){
+            return more.peek();
+        }
+        else{
+            return (more.peek() + less.peek())/2;
+        }
     }
     
     public int size(){
@@ -20,9 +40,28 @@ public class RunningMedian{
     }
     
     public String toString(){
-        return Arrays.toString(data);
+        return "Less: " + less + "\nMore: " + more;
     }
     
-    public void resize(){};
+    public static void main(String[]args){
+        RunningMedian test = new RunningMedian();
+        test.add(0.);
+        test.add(13.);
+        test.add(9.);
+        test.add(123.);
+        test.add(5.);
+        test.add(63.);
+        test.add(3.);
+        test.add(13.);
+        test.add(74.);
+        test.add(13.);
+        
+        
+        
+        
+        
+        System.out.println(test);
+        System.out.println(test.getMedian());
+    }
     
 }
