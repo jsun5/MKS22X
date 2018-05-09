72af2965469d4049dcb6ae06e8c2b593e3c103fe
jsun5 <jsun5@stuy.edu>
2018-05-09 14:50:54 -0400

    implement comparable

--- 14Frontier/Location.java
+++ 14Frontier/Location.java
@@ -1,11 +1,20 @@
-public class Location{
+public class Locationc implements Comparable<Location>{
     private int x,y;
     private Location previous;
+    private int distance;
 
     public Location(int _x, int _y, Location prev){
 	x = _x;
 	y = _y;
 	previous = prev;
+	distance = 0;
+    }
+
+    public Location(int _x, int _y, Location prev, int d){
+	x = _x;
+	y = _y;
+	previous = prev;
+	distance = d;
     }
 
     public int getX(){
@@ -18,5 +27,13 @@ public class Location{
 
     public Location getPrev(){
 	return previous;
+    }
+
+    public int getDistance(){
+	return distance;
+    }
+
+    public int compareTo(Location other){
+	return getDistance() - other.getDistance();
     }
 }
